7cb1f7c71d620467b0b50f3a485b108718aa8544
raghu <raghubalakrishnann@gmail.com>
2024-08-17 21:46:41 +0530

    feat : Add CurrencyPickerDialog _WIP

--- composeApp/src/commonMain/kotlin/presentation/screen/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screen/HomeScreen.kt
@@ -7,12 +7,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import data.remote.api.CurrencyApiServiceImpl
+import domain.model.CurrencyType
+import presentation.component.CurrencyPickerDialog
 import presentation.component.HomeHeader
 import ui.theme.surfaceColor
 
@@ -22,9 +25,27 @@ class HomeScreen : Screen {
 
         val viewModel = getScreenModel<HomeViewModel>()
         val rateStatus by viewModel.rateStatus
+        val allCurrencies = viewModel.allCurrencies
         val sourceCurrency by viewModel.sourceCurrency
         val targetCurrency by viewModel.targetCurrency
         var amount by rememberSaveable() { mutableStateOf(0.0) }
+        var selectedCurrencyCode :CurrencyType by remember {
+            mutableStateOf(CurrencyType.None)
+        }
+        var dialogOpened by remember { mutableStateOf(true) }
+
+        if(dialogOpened){
+            CurrencyPickerDialog(
+                currencies = allCurrencies,
+                currencyType = selectedCurrencyCode,
+                onPositiveClick = {
+                    dialogOpened = false
+                },
+                onDismiss = {
+                  dialogOpened = false
+                })
+        }
+
 
         Column(
             modifier = Modifier.fillMaxSize()
